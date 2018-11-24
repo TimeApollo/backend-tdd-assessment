a1326ba3e40d46c971a942ff66d28ba5f7ef7a62
TimeApollo <arnjcksn0845@yahoo.com>
2018-11-24 13:12:04 -0600

    finished argparse. working on making tests work

---
+++ echo.py
@@ -0,0 +1,30 @@
+"""
+TDD echo command line tool creation
+
+Using test driven development, the program implements the echo function
+with some added features
+
+Aaron Jackson
+TimeApollo
+"""
+
+__author__ = "TimeApollo"
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Perform transformation on input text.")
+    parser.add_argument("-u", "--upper", action="store_true",
+                        help="convert text to uppercase")
+    parser.add_argument("-l", "--lower", action="store_true",
+                        help="convert text to lowercase")
+    parser.add_argument("-t", "--title", action="store_true",
+                        help="convert text to titlecase")
+    parser.add_argument("text", help="text to be manipulated")
+
+    args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_echo.py
@@ -0,0 +1,22 @@
+import subprocess
+import unittest
+import echo
+
+
+class TestKatas(unittest.TestCase):
+    def test_help(self):
+        """ Running the program without arguments should show usage. """
+
+        # Run the command `python ./echo.py -h` in a separate process, then
+        # collect it's output.
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-h"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        usage = open("./USAGE", "r").read()
+
+        self.assertEquals(stdout, usage)
+
+
+if __name__ == '__main__':
+    unittest.main()
