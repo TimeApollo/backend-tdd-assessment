3ea77e9ea1dfec48db46f33793a7c4cb3a7676cf
TimeApollo <arnjcksn0845@yahoo.com>
2018-11-24 17:30:56 -0600

    finished all unit tests and program functionality

--- echo.py
+++ echo.py
@@ -17,9 +17,22 @@ import argparse
 import sys
 
 
+def upper_case(text):
+    return text.upper()
+
+
+def lower_case(text):
+    return text.lower()
+
+
+def title_case(text):
+    return text.title()
+
+
 def create_parser():
     """Creates and returns an argparse cmd line option parser"""
-    parser = argparse.ArgumentParser(description="Perform transformation on input text.")
+    parser = argparse.ArgumentParser(
+        description="Perform transformation on input text.")
     parser.add_argument("-u", "--upper", action="store_true",
                         help="convert text to uppercase")
     parser.add_argument("-l", "--lower", action="store_true",
@@ -28,13 +41,23 @@ def create_parser():
                         help="convert text to titlecase")
     parser.add_argument("text", help="text to be manipulated")
 
-    return parser.parse_args()
+    return parser
 
 
-def main():
+def main(args):
     """Implementation of echo"""
-    args = create_parser()
+    parser_args = create_parser().parse_args(args)
+    text = parser_args.text
+
+    if parser_args.upper:
+        text = upper_case(text)
+    if parser_args.lower:
+        text = lower_case(text)
+    if parser_args.title:
+        text = title_case(text)
+
+    print(text)
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
